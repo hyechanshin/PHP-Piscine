149056bb014fc0b6fb26e8bf1376baca5a155193
Stephen Shin <stephenshin@Stephens-Mac.local>
2020-06-26 13:18:28 -0700

    changeson ex00

--- day04/ex00/index.php
+++ day04/ex00/index.php
@@ -1,18 +1,16 @@
 <?php
     session_start();
-    if ($_GET["submit"] === "OK") {
-        $_SESSION["login"]=$_GET["login"];
-        $_SESSION["passwd"]=$_GET["passwd"];
+    if ($_GET['login'] && $_GET['passwd'] && $_GET['submit'] && $_GET['submit'] === "OK") {
+        $_SESSION["login"] = $_GET["login"];
+        $_SESSION["passwd"] = $_GET["passwd"];
     }
-    $_SESSION["newsession"]=$value;
-    echo $_SESSION["newsession"];
 ?>
 
 <html><body>
     <form action="index.php" method="GET">
-        Username: <input type='text' name="login" value="<?PHP echo $_SESSION['login'];?>" />
+        Username: <input type='text' name="login" value="<?php echo $_SESSION['login']; ?>" />
         <br />
-        Password: <input type="password" name="passwd" value="<?PHP echo $_SESSION['passwd'];?>"  />
-        <input type="submit" name = "submit" value = "OK" />
+        Password: <input type="password" name="passwd" value="<?php echo $_SESSION['passwd']; ?>" />
+        <input type="submit" name ="submit" value="OK" />
     </form>
 </body></html>
