2445b5a7164447c2205c930d2dbf09c6d07ef89f
Hyechan Shin <kirikeria@gmail.com>
2020-11-14 16:41:09 +0200

    edit

--- day04/ex02/modif.php
+++ day04/ex02/modif.php
@@ -1,23 +1,23 @@
 <?php
-     if ($_POST['login'] && $_POST['passwd'] && $_POST['submit'] &&  $_POST['submit'] === "OK" && file_exist('../private/passwd')) {
+    if ($_POST['login'] && $_POST['oldpw'] && $_POST['newpw'] && $_POST['submit'] && $_POST['submit'] === "OK") {
         $user_acc = unserialize(file_get_contents('../private/passwd'));
         if ($user_acc) {
             $exist = 0;
             foreach ($user_acc as $k => $v) {
-                if ($v['login'] === $_POST['login'] && $v['passwd'] === hash('whirlpool', $_POST['oldpw'])) { 
+                if ($v['login'] === $_POST['login'] && $v['passwd'] === hash('whirlpool', $_POST['oldpw'])) {
                     $exist = 1;
-                    $account[$k]['passwd'] =  hash('whirlpool', $_POST['newpw']);
+                    $user_acc[$k]['passwd'] =  hash('whirlpool', $_POST['newpw']);
                 }
             }
             if ($exist) {
                 file_put_contents('../private/passwd', serialize($user_acc));
-			    echo "OK\n";
+                echo "OK\n";
             } else {
                 echo "ERROR\n";
             }
         } else {
             echo "ERROR\n";
-            }
+        }
     } else {
         echo "ERROR\n";
     }
